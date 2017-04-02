36d271eab356926970eb91a6bbb249d0e1dccdc3
Adnieszka <watrucka@gmail.com>
2017-04-03 00:11:16 +0200

    Add files via upload

--- meanGA_camel6.R
+++ meanGA_camel6.R
@@ -6,27 +6,27 @@ meanGA <- function(population,iteration,crosing,mutant,mini,maxi)
     for ( i in 1:5)
     {  
         GA <- ga(type = "real-valued",
-            fitness =  function(x) -easom(x[1], x[2]),
+            fitness =  function(x) - camel6(x[1], x[2]),
                 min = mini, max = maxi, pcrossover = crosing, pmutation = mutant,
                 popSize = population, maxiter = iteration, run = 100)
         xMax[,i] <- GA@summary[,1]
         xMean[,i] <- GA@summary[,2]
-	summary(GA)
+		print(GA@solution)
  
     }
  
 return(list(max=xMax,min=xMean))
  
 }
-gaResult <- meanGA(100,100,0.5,0.1,c(-12,-12),c(10,10))
+gaResult <- meanGA(50,50,0.5,0.1,c(-8,-8),c(5,5))
 meanRowsMax <- rowMeans(gaResult$max )
 meanRowsMean <- rowMeans(gaResult$min)
 
-plot(meanRowsMax , type = "p",col="red",axes = FALSE, main = "Wartoœæ najlepszego osobnika po uœrednieniu",
+plot(meanRowsMax , type = "p",col="red", main = "Wartoœæ najlepszego osobnika po uœrednieniu",
 xlab=" Iteracja", ylab="Wartoœæ funkcji dopasowania")
 par(new = TRUE)
-plot(meanRowsMean,col="green", axes = FALSE, xlab = "", ylab = "")
+plot(meanRowsMean,col="green", xlab = "", ylab = "")
 legend("bottomright",  c("Wartosc najlepszego osobnika","Œrednia wartoœæ osobników"), lty=c(1,1), lwd=c(2.5,2.5),col=c("red","green"))
-axis(1, at = seq(0, 200, by = 2), las=2)
-axis(2, at = seq(0, 1.02, by = 0.05), las=2)
+#axis(1, at = seq(0, 200, by = 2), las=2)
+#axis(2, at = seq(0, 500, by = 1), las=2)
 grid (NULL,NULL, lty = 4, col = "cornsilk2")
