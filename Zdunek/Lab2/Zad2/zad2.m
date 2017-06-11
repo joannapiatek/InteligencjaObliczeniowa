a282361639313ecda0fd3900e678e76dfe9699e6
joannapiatek <fickle.joan@gmail.com>
2017-06-11 18:47:05 +0200

    [Lab2] Sprawko zad2

--- Zdunek/Lab2/Zad2/zad2.m
+++ Zdunek/Lab2/Zad2/zad2.m
@@ -6,12 +6,12 @@ I2 = 20;
 I3 = 30;
 J = 5;
 
-% Inicjalizacja
+% Inicjalizacja faktorow
 U1w = max(0, randn(I1, J));
 U2w = max(0, randn(I2, J));
 U3w = max(0, randn(I3, J));
 
-%% Syntetyczne obserwacje Y 
+% Syntetyczne obserwacje Y 
 I = zeros(J,J,J);
 for i = 1 : J
     I(i,i,i) = 1;   
@@ -20,21 +20,23 @@ end
 Y = ntimes(ntimes(ntimes(I, U1w, 1, 2), U2w, 1, 2), U3w, 1, 2);
 
 %% Algorytm ALS
-%% Inicjalizacja
+% Inicjalizacja faktorow
 U1 = randn(I1, J);
 U2 = randn(I2, J);
 U3 = randn(I3, J);
 
+% Matrycyzacja Y wzgledem poszczegolnych modow
 Y1 = reshape(permute(Y,[1,2,3]),size(Y,1),size(Y,2)*size(Y,3));
 Y2 = reshape(permute(Y,[2,1,3]),size(Y,2),size(Y,1)*size(Y,3));
 Y3 = reshape(permute(Y,[3,1,2]),size(Y,3),size(Y,1)*size(Y,2));
 
-% Update
+% Obliczenie wersji 2-D tensora Y
 Y_2d = reshape(Y,size(Y,1),size(Y,2)*size(Y,3));
 
-MaxIter = 30;
+MaxIter = 200;
 for k = 1: MaxIter
     
+    % Update naprzemienny faktorow i normalizacja
     A1 = max(0,(kr(U3,U2))');    
     U1 = max(0, (Y1*A1')/(A1*A1'));
     U1 = U1*(diag(1./sum(U1, 1)));
@@ -46,6 +48,8 @@ for k = 1: MaxIter
     A3 = max(0,(kr(U2,U1))');
     U3 = max(0, (Y3*A3')/(A3*A3'));
     
+    % Wyliczanie tensora Yk na dla k-tej iteracji
+    % na podstawie aktualnych U1, U2 i U3
     Yk = ntimes(ntimes(ntimes(I,U1,1,2),U2,1,2),U3,1,2);
     Yk_2d = reshape(Yk,size(Yk,1),size(Yk,2)*size(Yk,3));
     
@@ -53,7 +57,9 @@ for k = 1: MaxIter
     res(k) = norm(Y_2d - Yk_2d, 'fro')/norm(Y_2d, 'fro');
 end
 
+% Rysowanie wykresu bledu
 semilogy(res)
 
+% Jakosc estymacji
 mse = immse(Y, Yk);
 sir = CalcSIR(Y_2d, Yk_2d);
